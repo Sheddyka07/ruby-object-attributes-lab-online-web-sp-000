d84eea894294b423189cb12df4a14537c8de78be
Kelly Sheddy <jungaka07@gmail.com>
2018-11-08 18:41:33 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,4 +8,15 @@ class Dog
   def name=(new_name)
     @name=new_name
   end
+  
+  def intialize (breed)
+    @breed = breed
+  end
+  def breed
+    @name
+  end
+  def name=(new_name)
+    @name=new_name
+  end
+  
 end
